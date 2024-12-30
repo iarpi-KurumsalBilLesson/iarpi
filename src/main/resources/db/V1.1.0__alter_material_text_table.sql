0b6a4085f55a9fa1bfbf041ea7469174a7bdaf53
Adnan <adnan.ozturk@ogr.sakarya.edu.tr>
2024-12-30 12:55:19 +0300

    migration was edited

--- src/main/resources/db/V1.1.0__alter_material_text_table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE material_text
-    ADD material_head_id INT;
-
-ALTER TABLE material_text
-    ADD CONSTRAINT fk_material_head FOREIGN KEY (material_head_id) REFERENCES material_head (ID) ON DELETE CASCADE ;
